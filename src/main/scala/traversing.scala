023aa9905aa6790049288fbf84e45227946a0eeb
Freshia <freshiasackey@gmail.com>
2020-04-08 23:16:26 +0300

    Fixed null pointer exception

---
+++ src/main/scala/traversing.scala
@@ -0,0 +1,44 @@
+
+class transversing(start_location:Int){
+
+    val mygraph: graph = new graph();
+    val thestack: stack = new stack(20)
+    
+    def getadj_unvisited(v:Int,locations:Array[Vertex], adj_matrix: Array[Array[Int]], number_of_locations:Int ): Int = {
+        var present:Int = -1
+        for(j<-0 to number_of_locations - 1){
+            if(adj_matrix(v)(j) != 0 && locations(j).wasVisited == false){
+                    present = j
+            }
+        }
+        return present
+    }
+    def print_vertex(v: Int,locations:Array[Vertex]){
+        println(locations(v).vertex_name)
+    }
+    def dfs(locations: Array[Vertex], adj_matrix: Array[Array[Int]], number_of_locations:Int):Unit = {
+        
+        println("My start location")
+        locations(start_location).wasVisited = true
+        thestack.push(start_location)
+        print_vertex(start_location,locations)
+        while(!thestack.isEmpty){
+            val v:Int = getadj_unvisited(thestack.peek(),locations, adj_matrix, number_of_locations)
+
+            if(v == -1){
+                thestack.pop()
+            }
+            else{
+                locations(v).wasVisited = true
+                print_vertex(v,locations)
+                thestack.push(v)
+            }
+        }
+    }
+    def reset(locations: Array[Vertex], number_of_locations: Int):Unit = {
+        for(i<-0 to number_of_locations-1){
+            locations(i).wasVisited = false
+        }
+    }
+
+}
